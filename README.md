ef2916830d4a1446635a4303d7956175b702294f
Gal Angel <gal.angel@ironsrc.com>
2019-02-03 00:56:03 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,228 @@
+
+# Easy-INI - a simple way to manipulate INI files content
+### Table of Contents
+
+- [Getting Started](#getting-started)
+- [API](#class-methods)
+- [Good to know](#good-to-know)
+- [Use Cases](#use-case-examples)
+- [Author](#author)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+
+---
+## Getting Started
+why should you use this package?
+
+1. You don't want to manipulate an object and just use stright and forward methods (you can if you insist)
+2. You care about preserving comments
+3. You want want easy formatting and cool logic like merge and replace
+
+### basic example
+
+```javascript
+const INI = require('INI-master')
+const myINI = new INI('cool=awesome')
+myINI.putStringInSection('; so important','[WOW]')
+const giveItBack = createINIString()
+```
+---
+## Class Methods:
+
+**constructor(,{})**
+*Create ini object*
+- ``str``,
+- *optional:* ``defSec`` = 'DEFAULT_SECTION!',
+- *optional:* ``eol`` = require('os').EOL)
+- *optional:* ``autoTrim`` = true
+```javascript
+// You can start with an empty INI
+const myINI = new INI('')
+
+// By default the EOL is the taken from the OS, set it if needed
+const myINI = new INI('',{eol: '\r\n'})
+
+// DebSec is needed to represent the first lines that may not be under a section
+// By default it's: [DEFAULT_SECTION!] , if for some reason you actually use this name for a section, provide another to avoid unwanted behavior
+const myINI = new INI('',{defSec: 'NEXT_LEVEL_DEFAULT_SECTION'})
+```
+<br>
+
+**```createINIString```({})**
+*Make an ini string from the object, every param is optional and do what it says*
+- shouldTrim = false,
+- shouldFix = false,
+- cleanHashComments = false,
+- cleanBreakComment = false,
+- cleanEmptyLines = false,
+- noEmptySections = false,
+- noSections = false
+```javascript
+// To get true representation use without any parameter
+const newINIString = myINI.createINIString()
+```
+<br>
+
+**```getKeyIfExists```()**
+*Get line object by referance {type, key, val}*
+- inputKey
+```javascript
+// Changing the returned object will change the original
+myINI.getKeyIfExists('cool').val = 'ouch'
+// Returnes null if failed to find
+```
+<br>
+
+**```findAndChangeKeyIfExists```()**
+*finds a pair by key and replace value*
+- inputKey,
+- inputValue = ''
+```javascript
+// Same as using getKeyIfExists but without returning referance
+myINI.findAndChangeKeyIfExists('cool','OUCH')
+// Returnes true for success, otherwize false
+```
+<br>
+
+**```findAndRemoveKeyIfExists```()**
+*remove key value pair from object*
+- inputKey
+```javascript
+// Remove a pair (key=value) by matching the key with input string
+myINI.findAndRemoveKeyIfExists('cool')
+// Returnes true for success, otherwize false
+```
+<br>
+
+**```findAndRemoveSectionIfExists```()**
+*remove entire section from object*
+- sectionName
+```javascript
+// Remove an entire section
+myINI.findAndRemoveSectionIfExists('[DO_NOT_REMOVE]')
+// Returnes true for success, otherwize false
+```
+<br>
+
+**```putStringInSection```()**
+*adds a line to the end of a section*
+- string,
+- sectionName = this.defSecName
+```javascript
+// If section does not exist, will create it at the end
+myINI.putStringInSection('#comment','[BLAHBLAH]')
+// If the input is a key=value pair, and key exists in section , will change its value
+// Also true when no section is provided (will use default one)
+```
+<br>
+
+
+**```getLinesByMatch```()**
+*find all lines containing a string*
+- token
+```javascript
+// Will return an array with with all matches across all sections
+myINI.getLinesByMatch('#INCLUDE=')
+```
+<br>
+
+**```removeLineByMatch```()**
+*matches keys, values or comments*
+- token,
+- global = false,
+- _done = false (internal use)
+```javascript
+// Will return true if at least one line was removed, else false
+myINI.removeLineByMatch(';DUAH', true)
+```
+<br>
+
+**```findAndReplace```()**
+*searches for the token and replaces with the value if found*
+- token,
+- value = '',
+- global = false,
+- _done = false (internal use)
+```javascript
+// if global is false will change only the first occurrence
+myINI.findAndReplace('<<BASE_DOMAIN>>', 'mashu-mashu-mashu.com', true)
+// Will return true if at least one line was removed, else false
+```
+<br>
+
+**```solveDuplicates```()**
+*fixes ini object so and removes duplicate keys leaving first or last occurence*
+- preferFirstOccurrence = false
+```javascript
+// Will remove duplicate keys across the entire ini object
+myINI.solveDuplicates()
+// Returnes true when finished
+```
+<br>
+
+**```mergeWith```()**
+*merges with another ini object*
+- anotherINIObject
+- before = false
+```javascript
+// If before is true will place new values at the beginning of each section
+myINI.mergeWith(notMyINI)
+```
+<br>
+
+---
+
+## Good To Know:
+1. the INI class accepets a string input for the constructor ( not a path to an ini file )
+2. the default section is a representation for the first lines that are not under any section (could be the whole file)
+3. considers text only lines as garbage
+4. You can edit ```myINI.iniData``` directly
+5. line types:
+    - 0: empty line
+    - 1: hash comment
+    - 2: break comment
+    - 3: section
+    - 4: pair
+    - 5: garbage
+
+---
+
+## Use Case Examples:
+
+#### handling ini dependency
+```javascript
+const fs = require('fs')
+const INI = require('./INI')
+const productConfig = new INI(fs.readFileSync('./amazing_app_info.ini',{encoding: 'utf8'}))
+
+let includes
+while (includes = productConfig.getLinesByMatch("#INCLUDE")){
+    if(includes.length == 0) {break}
+    productConfig.removeLineByMatch('#INCLUDE', true)
+    for (const include of includes.reverse()) {
+        const includePath = include.split('=')[1]
+        const tempINI = new INI(fs.readFileSync(includePath, {encoding: 'utf8'}))
+        productConfig.mergeWith(tempINI, true)
+    }
+}
+productConfig.solveDuplicates()
+const finalConfig = productConfig.createINIString()
+fs.writeFileSync("./final.ini", finalConfig)
+```
+<br>
+
+---
+## Author
+
+* **[Gal Angel](gal0angel@gmail.com)**
+
+
+## License
+
+[GNU General Public License v3.0](/LICENCE.md)
+
+## Acknowledgments
+
+* stackOverflow
+* coffee
+* my cats
---
+++ index.js
@@ -0,0 +1,287 @@
+const   _EMPTY_LINE = 0,
+        _HASH_COMMENT = 1,
+        _BREAK_COMMENT = 2,
+        _SECTION = 3,
+        _PAIR = 4,
+        _GARBAGE = 5
+
+
+class INIClass{
+    constructor(str, {defaultSectionName = 'DEFAULT_SECTION!', eol = require('os').EOL, autoTrim = true} =  {defaultSectionName: 'DEFAULT_SECTION!', eol: require('os').EOL, autoTrim: true}) {
+        this.origin = str
+        this.autoTrim = autoTrim
+        this.eol = eol
+        this._defSecName = defaultSectionName
+        this._typeScheme = [
+            {'key': () => '', 'val': () => null},
+            {'key': l => l, 'val': () => null},
+            {'key': l => l, 'val': () => null},
+            {'key': l => l, 'val': () => null},
+            {'key': l => l.split('=')[0], 'val': l => l.split(/=(.+)/)[1]},
+            {'key': l => l, 'val': () => null}
+        ]
+        this._lineGens = [   //line, trim ,clean(comment garbage)
+            () => '',
+            (l,t) => t ? l.key.trim() : l.key,
+            (l,t) => t ? l.key.trim() : l.key,
+            (l,t) => t ? l.key.trim() : l.key,
+            (l,t) => t ? `${l.key.trim()}=${l.val.trim()}` : `${l.key}=${l.val}`,
+            (l,t,c) => c ? `;${t ? l.key.trim() : l.key}` : `${t ? l.key.trim() : l.key}`
+        ]
+        this._types = [
+            v => v.match(/^\s*$/) != null,              //     0: 'emptyLine',
+            v => v.match(/^ *#/) != null,               //     1: 'hashcomment',
+            v => v.match(/^ *;/) != null,               //     2: 'breakcomment',
+            v => v.match(/^\s*\[.*\]$/) != null,        //     3: 'section',
+            v => v.match(/^\s*([^#;]*\w+)=/) != null,   //     4: 'pair',
+            v => v.match(/^([^=#;]+\w$)/) != null       //     5: 'garbage'
+        ]
+        this.iniData = this._createINIData(this.origin)
+    }
+
+    _secArr(sec) {return sec[Object.keys(sec)[0]]}
+
+    _secName(sec) {return Object.keys(sec)[0]}
+
+    _getSection(obj, i) {return obj[i][Object.keys(obj[i])[0]]}
+
+    _findType(line) {for (let t in this._types) {if (this._types[t](line)) {return t}}}
+
+    _addByType(obj, type, key = '', val = '') {
+        if (type == _SECTION) {obj.push({[key]:[]})} else {
+            this._getSection(obj, obj.length -1).push({type, key, val})
+        }
+    }
+
+    _createINIData(string) {
+        const iniData = []
+        const lines = string.split(this.eol)
+        if (this._findType(lines[0]) != _SECTION) {iniData.push({[this._defSecName]:[]}) }
+        for (let l of lines) {
+            if(this.autoTrim) {l = l.trim()}
+            const lineType = this._findType(l)
+            try{
+                this._addByType(iniData, lineType, this._typeScheme[lineType].key(l), this._typeScheme[lineType].val(l))
+            } catch (e) {
+                console.log(l,e)
+                throw e
+            }
+        }
+        return iniData
+    }
+
+    createINIString({shouldTrim = false, shouldFix = false, cleanHashComments = false, cleanBreakComment = false, cleanEmptyLines = false, noEmptySections = false, noSections = false} =
+        {shouldTrim: false, shouldFix: false, cleanHashComments: false, cleanBreakComment: false, cleanEmptyLines: false, noEmptySections: false, noSections: false}) {
+        let output = '', curLine = ''
+        for (const section of this.iniData) {
+            let sectionString = '', contentString = ''
+            const sName = this._secName(section)
+            if (sName != this._defSecName && !noSections) {
+                sectionString += cleanEmptyLines ? this.eol : ''
+                sectionString += shouldTrim ? `${sName.trim()}${this.eol}` : `${sName}${this.eol}`
+            }
+            for(const l of this._secArr(section)) {
+                curLine = this._lineGens[l.type](l, shouldTrim, shouldFix)
+                if (cleanEmptyLines && this._findType(curLine) == _EMPTY_LINE) {continue}
+                if (cleanHashComments && this._findType(curLine) == _HASH_COMMENT) {continue}
+                if (cleanBreakComment && this._findType(curLine) == _BREAK_COMMENT) {continue}
+                contentString += `${curLine}${this.eol}`
+            }
+            if (noEmptySections && contentString == '') {continue}
+            output += sectionString + contentString
+        }
+        return output
+    }
+
+    getKeyIfExists(inputKey) {
+        for (const sec of this.iniData) {
+            for (let line of this._secArr(sec)) {
+                if(line.type == _PAIR && line.key.trim() == inputKey.trim()) {
+                    return line
+                }
+            }
+        }
+        return null
+    }
+
+    findAndChangeKeyIfExists(inputKey, inputValue = '') {
+        const res = this.getKeyIfExists(inputKey)
+        if (res) {
+            res.val = inputValue
+            return true
+        }
+        return false
+    }
+
+    findAndRemoveKeyIfExists(inputKey) {
+        for (const sec of this.iniData) {
+            for (const [index, line] of this._secArr(sec).entries()) {
+                if (line.type == _PAIR && line.key.trim() == inputKey.trim()) {
+                    this._secArr(sec).splice(index, 1)
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    findAndRemoveSectionIfExists(sectionName) {
+        for (const [index, section] of this.iniData.entries()) {
+            if (this._secName(section) == sectionName) {
+                this.iniData.splice(index, 1)
+                return true
+            }
+        }
+        return false
+    }
+
+    putStringInSection(string, sectionName = this._defSecName) {
+        let existingSec = false, valueAdded = false
+        const stringType = this._findType(string)
+        const stringObj = {
+            type: stringType,
+            key: this._typeScheme[stringType].key(string),
+            val: this._typeScheme[stringType].val(string)
+        }
+        for (const sec of this.iniData) {
+            const sName = this._secName(sec)
+            if (sectionName == sName.trim()) {
+                const lines = this._secArr(sec)
+                for (const line of lines) {
+                    if (line.key == stringObj.key && stringObj.type == _PAIR) {
+                        line.val = stringObj.val
+                        valueAdded = true
+                        break
+                    }
+                }
+                if (!valueAdded) {
+                    lines.push(stringObj)
+                    existingSec = true
+                    break
+                }
+            }
+        }
+        if (!existingSec && !valueAdded) {
+            this.iniData.push({[sectionName]:[]})
+            return this.putStringInSection(string, sectionName)
+        }
+        return true
+    }
+
+    getLinesByMatch(token) {
+        const resultLines = []
+        for (const sec of this.iniData) {
+            const sLines = this._secArr(sec)
+            for (const line of sLines) {
+                if(line.type == _PAIR) {
+                    if (line.val.indexOf(token) >= 0 || line.key.indexOf(token) >= 0) {
+                        resultLines.push(this._lineGens[line.type](line))
+                        continue
+                    }
+                }
+                if (line.key.indexOf(token) >= 0) {
+                    resultLines.push(this._lineGens[line.type](line))
+                    continue
+                }
+            }
+        }
+        return resultLines
+    }
+
+    removeLineByMatch(token, global = false, _done = false) {
+        for (const sec of this.iniData) {
+            const sLines = this._secArr(sec)
+            for (const [index, line] of sLines.entries()) {
+                if(line.type == _PAIR) {
+                    if (line.val.indexOf(token) >= 0 || line.key.indexOf(token) >= 0) {
+                        sLines.splice(index, 1)
+                        if (global) {return this.removeLineByMatch(token, global, true)}
+                        else {_done = true; break}
+                    }
+                }
+                if (line.key.indexOf(token) >= 0) {
+                    sLines.splice(index, 1)
+                    if (global) {return this.removeLineByMatch(token, global, true)}
+                    else {_done = true; break}
+                }
+            }
+        }
+        return _done
+    }
+
+    findAndReplace(token, value = '', global = false, _done = false) {
+        for (const sec of this.iniData) {
+            const sLines = this._secArr(sec)
+            for (const line of sLines) {
+                if(line.type == _PAIR) {
+                    if (line.val.indexOf(token) >= 0) {
+                        line.val = line.val.replace(token, value)
+                        if (global) {return this.findAndReplace(token, value, global, true)}
+                        else {_done = true; break}
+                    }
+                }
+                if (line.key.indexOf(token) >= 0) {
+                    line.key = line.key.replace(token, value)
+                    if (global) {return this.findAndReplace(token, value, global, true)}
+                    else {_done = true; break}
+                }
+            }
+        }
+        return _done
+    }
+
+    solveDuplicates(preferFirstOccurrence = false) {
+        for (const sec of this.iniData) {
+            const sLines = this._secArr(sec)
+            for (const [index, line] of sLines.entries()) {
+                if(line.type == _PAIR) {
+                    if (preferFirstOccurrence) {
+                        const tempKey = line.key
+                        line.key = 'TeMpKeY'
+                        while (this.findAndRemoveKeyIfExists(tempKey)) {}
+                        line.key = tempKey
+                    } else {
+                        const tempKey = line.key
+                        line.key = 'TeMpKeY'
+                        if (this.getKeyIfExists(tempKey)) {
+                            sLines.splice(index, 1)
+                            return this.solveDuplicates(preferFirstOccurrence)
+                        } else {
+                            line.key = tempKey
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    }
+
+    mergeWith(anotherINIObject, before = false) {
+        const otherINIData = anotherINIObject.iniData
+        let sectionFound
+        for (const s2 of otherINIData) {
+            sectionFound = false
+            const s2Name = this._secName(s2), s2Lines = this._secArr(s2)
+            for (const s of this.iniData) {
+                const sName = this._secName(s), sLines = this._secArr(s)
+                if (s2Name == sName) {
+                    if (before) {sLines.unshift(...s2Lines.map(a => ({...a})))}
+                    else {sLines.push(...s2Lines.map(a => ({...a})))}
+                    sectionFound = true
+                    break
+                }
+            }
+            if (!sectionFound) {
+                if (before) {
+                    this.iniData.splice(1, 0, {[s2Name]:[]});
+                    this.iniData[1][s2Name].push(...s2Lines.map(a => ({...a})))
+                } else {
+                    this.iniData.push({[s2Name]:[]})
+                    this.iniData[this.iniData.length -1][s2Name].push(...s2Lines.map(a => ({...a})))
+                }
+            }
+        }
+        return true
+    }
+}
+module.exports = INIClass
